6edf83f3582e332ee3a11e75be97ba81e9302713
Patrick Wu <magetron1@gmail.com>
2020-10-10 15:30:47 +0100

    finish printing IP

--- sw/main.cc
+++ sw/main.cc
@@ -1,4 +1,7 @@
 #define BUFFER_SIZE 65536
+#define IP_ETHERTYPE 0x0800
+#define TCP_IP_PROTO 0x06
+#define UDP_IP_PROTO 0x11
 
 #include <sys/socket.h>
 #include <linux/if_packet.h>
@@ -32,6 +35,32 @@ int main () {
     printf("\t|-Source Address : %.2X-%.2X-%.2X-%.2X-%.2X-%.2X\n", eth->h_source[0],eth->h_source[1],eth->h_source[2],eth->h_source[3],eth->h_source[4],eth->h_source[5]);
     printf("\t|-Destination Address : %.2X-%.2X-%.2X-%.2X-%.2X-%.2X\n", eth->h_dest[0],eth->h_dest[1],eth->h_dest[2],eth->h_dest[3],eth->h_dest[4],eth->h_dest[5]);
     printf("\t|-Protocol : 0x%04X\n", ntohs(eth->h_proto));
+
+    if (ntohs(eth->h_proto) == IP_ETHERTYPE) {
+      iphdr* ip = reinterpret_cast<iphdr*>(eth + 1);
+      in_addr src; src.s_addr = ip->saddr;
+      in_addr dst; dst.s_addr = ip->daddr;
+      printf("IP Header\n");
+      printf("\t|-Version : %d\n",(uint32_t)ip->version);
+      printf("\t|-Internet Header Length : %d DWORDS or %d Bytes\n", (uint32_t)ip->ihl,((uint32_t)(ip->ihl))*4);
+      printf("\t|-Type Of Service : %d\n", (uint32_t)ip->tos);
+      printf("\t|-Total Length : %d Bytes\n", ntohs(ip->tot_len));
+      printf("\t|-Identification : %d\n", ntohs(ip->id));
+      printf("\t|-Time To Live : %d\n", (uint32_t)ip->ttl);
+      printf("\t|-Protocol : %d\n",(uint32_t)ip->protocol);
+      printf("\t|-Header Checksum : %d\n", ntohs(ip->check));
+      printf("\t|-Source IP : %s\n", inet_ntoa(src));
+      printf("\t|-Destination IP : %s\n", inet_ntoa(dst));
+
+      switch (ip->protocol) {
+        case TCP_IP_PROTO:
+          printf("TCP Header\n");
+          break;
+        case UDP_IP_PROTO:
+          printf("UDP Header\n");
+          break;
+      }
+    }
 
   }
 
