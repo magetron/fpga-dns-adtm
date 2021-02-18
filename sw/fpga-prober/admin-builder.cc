93c4bbbe24b68facd1ba4e7ccf3b07fba6523e9c
Patrick Wu <magetron1@gmail.com>
2021-02-18 15:02:07 +0000

    provide correct admin builder

--- sw/fpga-prober/admin-builder.cc
+++ sw/fpga-prober/admin-builder.cc
@@ -7,11 +7,30 @@ FILE* file;
 uint64_t buf = 0;
 uint8_t bits_clean = 64;
 
+uint64_t reverse_bits_64 (uint64_t n) {
+  size_t s = sizeof(n) * 8;
+  uint64_t mask = ~0;
+  while ((s >>= 1) > 0) {
+    mask ^= (mask << s);
+    n = ((n >> s) & mask) | ((n << s) & ~mask);
+  }
+  return n;
+}
+
+uint64_t reverse_bits_byte_64 (uint64_t n) {
+  uint8_t* p = reinterpret_cast<uint8_t*>(&n);
+  for (uint8_t i = 0; i < 8; i++, p++) {
+    *p = (*p * 0x0202020202ULL & 0x010884422010ULL) % 1023;
+  }
+  return n;
+}
+
 void write_buf (uint64_t info, uint8_t bits) {
+  info = reverse_bits_64(info) >> (64 - bits);
   if (bits == 0) {
     // dump all buf to file, ready to close
     uint8_t bytes_to_write = ((64 - bits_clean) + 7) / 8;
-    buf = __bswap_64(buf);
+    buf = reverse_bits_byte_64(__bswap_64(buf));
     fwrite(&buf, bytes_to_write, 1, file);
   } else if (bits_clean >= bits) {
     // normal case
@@ -20,7 +39,7 @@ void write_buf (uint64_t info, uint8_t bits) {
     // write partially, write to file, write the other part
     uint8_t bits_after_write = bits - bits_clean;
     buf |= (info >> bits_after_write);
-    buf = __bswap_64(buf);
+    buf = reverse_bits_byte_64(__bswap_64(buf));
     fwrite(&buf, sizeof(buf), 1, file);
     bits_clean = 64 - bits_after_write;
     buf = (info & ((1 << (bits_after_write + 1)) - 1)) << bits_clean;
@@ -47,12 +66,12 @@ int main (int argc, char** argv) {
   f.srcMACBW = 1;
   f.srcMACLength = 1;
 
-  f.srcMACList[0].byte[0] = 0xae;
-  f.srcMACList[0].byte[1] = 0x29;
-  f.srcMACList[0].byte[2] = 0x5f;
-  f.srcMACList[0].byte[3] = 0x29;
-  f.srcMACList[0].byte[4] = 0x0c;
-  f.srcMACList[0].byte[5] = 0x00;
+  f.srcMACList[0].byte[0] = 0x00;
+  f.srcMACList[0].byte[1] = 0x0c;
+  f.srcMACList[0].byte[2] = 0x29;
+  f.srcMACList[0].byte[3] = 0x5f;
+  f.srcMACList[0].byte[4] = 0x29;
+  f.srcMACList[0].byte[5] = 0xaf;
   for (size_t i = 1; i < FILTER_DEPTH; i++) {
     for (size_t j = 0; j < 6; j++) f.srcMACList[i].byte[j] = 0x00;
   }
