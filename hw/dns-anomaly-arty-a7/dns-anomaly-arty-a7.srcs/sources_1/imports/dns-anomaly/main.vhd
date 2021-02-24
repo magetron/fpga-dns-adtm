276216723c80a702e6939cb8a3303ccdeca783ea
Patrick Wu <magetron1@gmail.com>
2021-02-24 23:41:32 +0000

    giving up on 100MHz clock, using 50MHz clock

--- hw/dns-anomaly-arty-a7/dns-anomaly-arty-a7.srcs/sources_1/imports/dns-anomaly/main.vhd
+++ hw/dns-anomaly-arty-a7/dns-anomaly-arty-a7.srcs/sources_1/imports/dns-anomaly/main.vhd
@@ -136,7 +136,7 @@ BEGIN
   
   fifo_receive : fifo_rcv PORT MAP(
     wclk => E_RX_CLK,
-    rclk => clk,
+    rclk => clk50,
     w_en => el_rcv_dv_phy,
     w_data => el_rcv_data_phy,
     r_en => el_rcv_ack_buf,
@@ -154,7 +154,7 @@ BEGIN
   );
   
   fifo_send : fifo_snd PORT MAP(
-    wclk => clk,
+    wclk => clk50,
     rclk => E_TX_CLK,
     w_en => el_snd_en_buf,
     w_data => el_snd_data_buf,
@@ -164,7 +164,7 @@ BEGIN
   );
 
   core : io PORT MAP(
-    clk => clk,
+    clk => clk50,
     -- Data received.
     el_rcv_data => el_rcv_data_buf,
     el_rcv_dv => el_rcv_dv_buf,
