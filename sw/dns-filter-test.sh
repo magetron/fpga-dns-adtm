fd695b7cea1d2844d634a9d42caf5239d89db6ab
Patrick Wu <magetron1@gmail.com>
2021-03-07 04:47:59 +0000

    add dns filter test script

---
+++ sw/dns-filter-test.sh
@@ -0,0 +1,8 @@
+sudo ./pkt-sender.out -t 10 -s once -m 00:0c:29:5f:29:ae,00:0a:35:00:00:ff -i 0.0.0.0,250.250.250.251 -u 12345,23456 -r apple.bin
+sudo ./pkt-sender.out -t 10 -s once -m 00:0c:29:5f:29:ae,00:0a:35:00:00:ff -i 0.0.0.0,250.250.250.251 -u 12345,23456 -r apple-simple.bin
+sudo ./pkt-sender.out -t 10 -s once -m 00:0c:29:5f:29:ae,00:0a:35:00:00:ff -i 0.0.0.0,250.250.250.251 -u 12345,23456 -r apple-complicated.bin
+sudo ./pkt-sender.out -t 10 -s once -m 00:0c:29:5f:29:ae,00:0a:35:00:00:ff -i 0.0.0.0,250.250.250.251 -u 12345,23456 -r dns.bin
+sudo ./pkt-sender.out -t 10 -s once -m 00:0c:29:5f:29:ae,00:0a:35:00:00:ff -i 0.0.0.0,250.250.250.251 -u 12345,23456 -r google.bin
+sudo ./pkt-sender.out -t 10 -s once -m 00:0c:29:5f:29:ae,00:0a:35:00:00:ff -i 0.0.0.0,250.250.250.251 -u 12345,23456 -r google-simple.bin
+sudo ./pkt-sender.out -t 10 -s once -m 00:0c:29:5f:29:ae,00:0a:35:00:00:ff -i 0.0.0.0,250.250.250.251 -u 12345,23456 -r google-complicated.bin
+sudo ./pkt-sender.out -t 10 -s once -m 00:0c:29:5f:29:ae,00:0a:35:00:00:ff -i 0.0.0.0,250.250.250.251 -u 12345,23456 -r admin.bin
