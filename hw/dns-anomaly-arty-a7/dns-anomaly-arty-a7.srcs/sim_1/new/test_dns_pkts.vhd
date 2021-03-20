f87fba76f45e90081b7880e895f2d3b64f37fdd8
Patrick Wu <magetron1@gmail.com>
2021-03-20 16:39:55 +0000

    add template for next test files

---
+++ hw/dns-anomaly-arty-a7/dns-anomaly-arty-a7.srcs/sim_1/new/test_dns_pkts.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 03/20/2021 04:39:17 PM
+-- Design Name: 
+-- Module Name: test_dns_pkts - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity test_dns_pkts is
+--  Port ( );
+end test_dns_pkts;
+
+architecture Behavioral of test_dns_pkts is
+
+begin
+
+
+end Behavioral;
---
+++ hw/dns-anomaly-arty-a7/dns-anomaly-arty-a7.srcs/sim_1/new/test_dstmac_pkts.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 03/20/2021 04:38:14 PM
+-- Design Name: 
+-- Module Name: test_dstmac_pkts - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity test_dstmac_pkts is
+--  Port ( );
+end test_dstmac_pkts;
+
+architecture Behavioral of test_dstmac_pkts is
+
+begin
+
+
+end Behavioral;
