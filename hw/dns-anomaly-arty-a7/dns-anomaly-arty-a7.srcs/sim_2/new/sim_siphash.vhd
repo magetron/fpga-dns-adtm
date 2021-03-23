03dc7e80828315c8f21f382f5c2e6ff61787a586
Patrick Wu <magetron1@gmail.com>
2021-03-23 23:07:32 +0000

    add new siphash simulation files for verification

---
+++ hw/dns-anomaly-arty-a7/dns-anomaly-arty-a7.srcs/sim_2/new/sim_siphash.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 03/23/2021 11:06:49 PM
+-- Design Name: 
+-- Module Name: sim_siphash - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity sim_siphash is
+--  Port ( );
+end sim_siphash;
+
+architecture Behavioral of sim_siphash is
+
+begin
+
+
+end Behavioral;
