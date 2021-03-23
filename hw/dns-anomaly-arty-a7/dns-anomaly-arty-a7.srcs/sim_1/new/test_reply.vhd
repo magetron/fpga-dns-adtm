6eeafeff0d9673a46c88e363e1d9d576b2071de7
Patrick Wu <magetron1@gmail.com>
2021-03-23 07:34:19 +0000

    update empty template for vhdl test file

---
+++ hw/dns-anomaly-arty-a7/dns-anomaly-arty-a7.srcs/sim_1/new/test_reply.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 03/23/2021 07:33:44 AM
+-- Design Name: 
+-- Module Name: test_reply - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity test_reply is
+--  Port ( );
+end test_reply;
+
+architecture Behavioral of test_reply is
+
+begin
+
+
+end Behavioral;
