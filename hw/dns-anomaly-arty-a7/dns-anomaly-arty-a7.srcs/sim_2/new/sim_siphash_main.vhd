0c0c7b4a3f2d260766f1f7c803bcc73dee570e07
Patrick Wu <magetron1@gmail.com>
2021-03-24 03:56:14 +0000

    add using in sim_siphash_main

--- hw/dns-anomaly-arty-a7/dns-anomaly-arty-a7.srcs/sim_2/new/sim_siphash_main.vhd
+++ hw/dns-anomaly-arty-a7/dns-anomaly-arty-a7.srcs/sim_2/new/sim_siphash_main.vhd
@@ -1,15 +1,42 @@
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
 
+LIBRARY work;
+USE work.sim_siphash.ALL;
+
 ENTITY sim_siphash_main IS
 END sim_siphash_main;
 
 ARCHITECTURE rtl OF sim_siphash_main IS
+  COMPONENT sim_siphash
+    PORT (
+      clk : IN STD_LOGIC;
+      in_key : IN STD_LOGIC_VECTOR(127 DOWNTO 0);
+      in_data : IN STD_LOGIC_VECTOR(959 DOWNTO 0);
+      in_start : IN STD_LOGIC;
+      out_data : OUT STD_LOGIC_VECTOR(63 DOWNTO 0);
+      out_ready : OUT STD_LOGIC;
+    );
+  END COMPONENT;
 
-SIGNAL clk : STD_LOGIC := '0';
-CONSTANT clk_period : TIME := 10 ns;
+  SIGNAL clk : STD_LOGIC := '0';
+  SIGNAL IN_KEY : IN STD_LOGIC_VECTOR(127 DOWNTO 0) := (OTHERS => '0');
+  SIGNAL IN_DATA : IN STD_LOGIC_VECTOR(959 DOWNTO 0) := (OTHERS => '0');
+  SIGNAL IN_START : IN STD_LOGIC := '0';
 
+  SIGNAL OUT_DATA : OUT STD_LOGIC_VECTOR(63 DOWNTO 0) := (OTHERS => '0');
+  SIGNAL OUT_READY : OUT STD_LOGIC := '0';
+
+  CONSTANT clk_period : TIME := 10 ns;
 BEGIN
+
+  hash : sim_siphash PORT MAP(
+    clk => clk,
+    in_key => IN_KEY,
+    in_data => IN_DATA,
+    out_data => OUT_DATA,
+    out_ready => OUT_READY
+  )
 
   -- Clock process definitions
   clk_process : PROCESS
@@ -27,6 +54,7 @@ BEGIN
     WAIT FOR 100 ns;
 
     WAIT FOR clk_period * 10;
+
 
   END PROCESS;
 
