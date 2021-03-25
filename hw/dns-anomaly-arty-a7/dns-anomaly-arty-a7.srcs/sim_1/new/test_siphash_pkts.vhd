6889beb5f28c218618e1d812706fbc0d05dfe1a0
Patrick Wu <magetron1@gmail.com>
2021-03-25 20:52:45 +0000

    add siphash ptk test bench

---
+++ hw/dns-anomaly-arty-a7/dns-anomaly-arty-a7.srcs/sim_1/new/test_siphash_pkts.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 03/25/2021 08:52:22 PM
+-- Design Name: 
+-- Module Name: test_siphash_pkts - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity test_siphash_pkts is
+--  Port ( );
+end test_siphash_pkts;
+
+architecture Behavioral of test_siphash_pkts is
+
+begin
+
+
+end Behavioral;
